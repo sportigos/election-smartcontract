c7a80b38fef96cffbbaac8d7875d5374f9021db1
LeonidAlexeyev <sportigo1120@gmail.com>
2021-06-07 12:31:28 +0900

    vote func added and some test added

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -17,6 +17,9 @@ contract Election {
     // Store Candidates Count
     uint public candidatesCount;
 
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+
     // Constructor
     constructor () public {
         candidate = "Candidate 1";
@@ -28,5 +31,19 @@ contract Election {
     function addCandidate (string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
     }
 }
