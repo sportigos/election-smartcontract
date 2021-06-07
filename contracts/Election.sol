7648dd889c58621aea09d112cb4c18a19a44d891
LeonidAlexeyev <sportigo1120@gmail.com>
2021-06-07 11:06:20 +0900

    Election contract updated and test added

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,32 @@
 pragma solidity ^0.5.0;
 
 contract Election {
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
     // Read/write candidate
     string public candidate;
+
+    // Read/write Candidates
+    mapping(uint => Candidate) public candidates;
+
+    // Store Candidates Count
+    uint public candidatesCount;
 
     // Constructor
     constructor () public {
         candidate = "Candidate 1";
+
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
